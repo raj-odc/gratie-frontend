7c7986cb4fd6892a5b1a1de5b428b7e9c0491997
mucks <mail@mucks.dev>
2023-03-12 19:43:57 +0700

    added basic ci/cd

--- src/gratie_solana_contract/gratie_solana_contract.ts
+++ src/gratie_solana_contract/gratie_solana_contract.ts
@@ -3,7 +3,7 @@ import { Connection, PublicKey } from "@solana/web3.js";
 import { GratieSolana } from "./types/gratie_solana";
 import idl from "./idl/gratie_solana.json";
 
-const NETWORK = "http://localhost:8899";
+const NETWORK = process.env.NODE_ENV === 'development' ? "http://localhost:8899" : "https://api.devnet.solana.com";
 
 export const connectToGratieSolanaContract = async (): Promise<anchor.Program<GratieSolana>> => {
   const connection = new Connection(NETWORK, "processed");
